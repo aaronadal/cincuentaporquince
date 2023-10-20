044a8d8bb8e05deb2fcc5f4d613c0d4a8bc18849
Aarón Nadal <aaronadal.dev@gmail.com>
2023-10-20 23:44:08 +0200

    upload custom contest data in json

--- src/model/Contest.ts
+++ src/model/Contest.ts
@@ -3,3 +3,47 @@ import type { Question } from './Question'
 export interface Contest {
   questions: Question[]
 }
+
+export function ensureContestValid(contest: any) {
+  if(typeof contest !== 'object' || !('questions' in contest) || !Array.isArray(contest.questions)) {
+    throw new Error('Debe ser un objeto con un atributo "questions" de tipo array');
+  }
+
+  contest.questions.forEach((question: any, index: number) => {
+    if(typeof question !== 'object') {
+      throw new Error(`La pregunta en la posición ${index} debe ser un objeto`);
+    }
+
+    if(!('text' in question)) {
+      throw new Error(`La pregunta en la posición ${index} debe tener una propiedad "text"`);
+    }
+
+    if(!('answers' in question) || !Array.isArray(question.answers)) {
+      throw new Error(`La pregunta en la posición ${index} debe tener una propiedad "answers" de tipo array`);
+    }
+
+    let correctCount = 0;
+    question.answers.forEach((answer: any, index: number) => {
+      if(typeof answer !== 'object') {
+        throw new Error(`La respuesta ${index} de la pregunta en la posición ${index} debe ser un objeto`);
+      }
+
+      if(!('text' in answer)) {
+        throw new Error(`La respuesta ${index} de la pregunta en la posición ${index} debe tener una propiedad "text"`);
+      }
+
+      if(!('correct' in answer)) {
+        throw new Error(`La respuesta ${index} de la pregunta en la posición ${index} debe tener una propiedad "correct"`);
+      }
+
+      correctCount = correctCount + (answer.correct ? 1 : 0);
+    });
+
+    if(correctCount === 0) {
+      throw new Error(`La pregunta en la posición ${index} no tiene ninguna respuesta marcada como correcta`);
+    }
+    else if(correctCount > 1) {
+      throw new Error(`La pregunta en la posición ${index} tiene ${correctCount} respuestas marcadas como correctas`);
+    }
+  });
+}
--- src/views/ConfigView.vue
+++ src/views/ConfigView.vue
@@ -1,11 +1,52 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue'
-import { useRouter } from 'vue-router';
+import { ensureContestValid, type Contest } from '@/model/Contest'
+import { useContestStore } from '@/stores/contest';
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-function onUploadContest() {
+const { setContest } = useContestStore()
+
+const file = ref<HTMLInputElement>();
+const error = ref('')
+const preparing = ref(false)
+
+async function parseJsonFile(file: File): Promise<Contest> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader()
+    reader.onload = (evt) => resolve(JSON.parse(evt.target?.result?.toString() || '{}'))
+    reader.onerror = (error) => reject(error)
+
+    reader.readAsText(file)
+  })
+}
+
+async function onUploadContest() {
+  error.value = ''
+
+  const json = file.value?.files?.[0];
+  if(!json) {
+    return;
+  }
+
+  const newContest = await parseJsonFile(json);
+  try {
+    ensureContestValid(newContest);
+  }
+  catch(e) {
+    error.value = `${e}`;
+    return;
+  }
+
+  preparing.value = true;
+  setContest(newContest)
   router.push({ name: 'home' })
+}
+
+function retry() {
+  error.value = ''
 }
 </script>
 
@@ -13,7 +54,8 @@ function onUploadContest() {
   <div class="page">
     <Header />
     <div>
-      <div class="text-flow">
+      <div v-if="preparing">Preparando partida...</div>
+      <div v-else class="text-flow">
         <h1>¡Configura tu cincuentaporquince!</h1>
 
         <p>
@@ -32,7 +74,12 @@ function onUploadContest() {
 
         <p>Una vez tengas listo el fichero, guarda los cambios y súbelo a continuación:</p>
 
-        <input style="margin-top: 4rem;" type="file" accept="application/json" @change="onUploadContest" />
+        <p v-if="error" style="color: var(--app-color-red)">
+          {{ error }}
+          <br />
+          <a @click="retry">REINTENTAR</a>
+        </p>
+        <input v-else ref="file" style="margin-top: 4rem;" type="file" accept="application/json" @change="onUploadContest" />
       </div>
     </div>
   </div>
